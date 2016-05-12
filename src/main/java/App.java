3bcdba73780ca7cac8aa8f694c79bd0f89dd0de5
Jacob Hefley <jacobhefley3@gmail.com>
2016-05-11 19:04:20 -0700

    wrong does not meet requirements

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,9 +5,56 @@ import static spark.Spark.*;
 import org.sql2o.*;
 
 public class App {
-
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+
+  get("/", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    List<Stylist> stylists = Stylist.all();
+    model.put("stylists", stylists);
+    model.put("template", "templates/index.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  // get("/stylists", (request, response) -> {
+  //   HashMap<String, Object> model = new HashMap<String, Object>();
+  //   List<Stylist> stylists = Stylist.all();
+  //   model.put("stylists" ,Stylist.all());
+  //   model.put("template", "templates/stylists.vtl");
+  //   return new ModelAndView(model, layout);
+  // }, new VelocityTemplateEngine());
+
+  get("/clients", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    model.put("clients", Client.all());
+    model.put("template", "templates/clients.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+
+  post("/clients", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    List<Stylist> stylists = Stylist.all();
+    int stylist2add2 = Integer.parseInt(request.queryParams("stylist2add2"));
+    String clientName = request.queryParams("newClient");
+    Client newClient = new Client(clientName, stylist2add2);
+    newClient.save();
+    model.put("clients", Client.all());
+    model.put("template", "templates/clients.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  post("/stylists", (request, response) -> {
+    HashMap<String, Object> model = new HashMap<String, Object>();
+    List<Stylist> stylists = Stylist.all();
+    String newstylist = request.queryParams("newstylist");
+    Stylist newStylist = new Stylist(newstylist);
+    newStylist.save();
+    model.put("stylists", Stylist.all());
+    model.put("template", "templates/stylists.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
   }
 }
--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -3,4 +3,69 @@ import org.sql2o.*;
 
 public class Stylist {
 
+  private String name;
+  private int client_id;
+  private int id;
+
+  @Override
+  public boolean equals(Object otherStylist){
+    if(!(otherStylist instanceof Stylist)){
+      return false;
+    }else {
+      Stylist newStylist = (Stylist) otherStylist;
+      return this.getName().equals(newStylist.getName()) &&
+      this.getId() == newStylist.getId();
+    }
+  }
+  public Stylist(String name){
+  	this.name = name;
+    this.id = 0;
+  }
+  public Stylist(String name, int id){
+  	this.name = name;
+    this.id = id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public static List<Stylist> all() {
+    String sql = "SELECT id, name FROM stylists";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Stylist.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO stylists(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Stylist find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM stylists where id=:id";
+      Stylist stylist = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Stylist.class);
+      return stylist;
+    }
+  }
+ public List<Client> getClients() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM clients where stylist_id=:id";
+    return con.createQuery(sql)
+      .addParameter("id", this.id)
+      .executeAndFetch(Client.class);
+  }
+}
 }
