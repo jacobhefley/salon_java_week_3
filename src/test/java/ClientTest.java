e05b74098af8c9fdcb25f49f54870675fbea6742
Jacob Hefley <jacobhefley3@gmail.com>
2016-05-17 15:06:02 -0700

    Client Constructor and get methods pass

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -9,8 +9,18 @@ public class ClientTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
-  public void Client_emptyAtStart_0() {
+  public void ClientList_emptyAtStart_0() {
     assertEquals(0, Client.all().size());
+  }
+
+  @Test
+  public void testClient_ConstructorObjectInstantiation_and_getPropertiesMethods() {
+    Client newClient = new Client("Smith, John", 1);
+    String expectedName = "Smith, John";
+    int expectedStylistID = 1;
+    assertEquals(true, newClient instanceof Client);
+    assertEquals(expectedName, newClient.getName());
+    assertEquals(expectedStylistID, newClient.getStylistId());
   }
 
 }
