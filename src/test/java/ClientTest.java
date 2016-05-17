4fd5e049c7d4a23fcd90b58b4be395e8afc508a8
Jacob Hefley <jacobhefley3@gmail.com>
2016-05-17 14:41:00 -0700

    Client_emptyAtStart passing, the database hair_salon table clients has no data

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,9 +1,16 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.*;
 
 public class ClientTest {
+
   @Rule
   public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Client_emptyAtStart_0() {
+    assertEquals(0, Client.all().size());
+  }
 
 }
