40a601b9f640a3b5697cfc411e26c6ca34b1549a
Jacob Hefley <jacobhefley3@gmail.com>
2016-05-17 13:57:27 -0700

    rootTest working, can go to index page

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,31 +1,31 @@
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import org.sql2o.*;
-//
-// import static org.assertj.core.api.Assertions.assertThat;
-// import static org.fluentlenium.core.filter.FilterConstructor.*;
-//
-// public class AppTest extends FluentTest {
-//
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//
-//   @Override
-//   public WebDriver getDefaultDriver() {
-//     return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
+import org.sql2o.*;
+import org.junit.*;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.junit.Assert.*;
 
-	// @Test
-	// public void rootTest() {
-	// 	goTo("http://localhost:4567/");
-	// 	assertThat(pageSource()).contains("My Salon");
-	// }
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
 
-// }
+  @Override
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("0NE ST0P BarberShop T00LS");
+  }
+}
